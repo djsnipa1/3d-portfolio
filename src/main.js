d34e7b3dc921683b8cfc1305896a9fb102bd8a43
Chad Boyce <chadboyce@gmail.com>
2021-12-22 10:03:31 +0000

    added html and css

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
@@ -32,11 +33,12 @@ pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// Helpers
+// const lightHelper = new THREE.PointLightHelper(pointLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -63,7 +65,7 @@ const chadTexture = new THREE.TextureLoader().load(
 );
 
 const chad = new THREE.Mesh(
-  new THREE.BoxGeometry(5, 5, 5),
+  new THREE.BoxGeometry(3, 3, 3),
   new THREE.MeshBasicMaterial({ map: chadTexture })
 );
 
@@ -75,31 +77,56 @@ const moonTexture = new THREE.TextureLoader().load(moonTextureImage);
 const moonBumomap = new THREE.TextureLoader().load(moonBumpmapImage);
 
 const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(10, 32, 32),
+  new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
     bumpMap: moonBumomap,
   })
 );
 
+chad.position.z = -10;
+chad.position.x = 5;
+
+// moon.position.z = 30;
+// moon.position.setX(-10);
+
 moon.position.x = -15;
 moon.position.z = -20;
 moon.position.y = 5;
 scene.add(moon);
 
-const gui = new GUI();
-const chadFolder = gui.addFolder("chad");
-chadFolder.add(chad.rotation, "x", 0, Math.PI * 2);
-chadFolder.add(chad.rotation, "y", 0, Math.PI * 2);
-chadFolder.add(chad.rotation, "z", 0, Math.PI * 2);
-chadFolder.open();
-const cameraFolder = gui.addFolder("camera");
-cameraFolder.add(camera.position, "z", 0, 20);
+// const gui = new GUI();
+// const chadFolder = gui.addFolder("chad");
+// chadFolder.add(chad.rotation, "x", 0, Math.PI * 2);
+// chadFolder.add(chad.rotation, "y", 0, Math.PI * 2);
+// chadFolder.add(chad.rotation, "z", 0, Math.PI * 2);
+// // chadFolder.open();
+// const cameraFolder = gui.addFolder("camera");
+// cameraFolder.add(camera.position, "z", 0, 20);
+// // cameraFoler.open();
 
-var axis = new THREE.Vector3(1, 0, 1).normalize();
-var speed = 0.01;
+// var axis = new THREE.Vector3(1, 0, 1).normalize();
+// var speed = 0.01;
 
 //const axisFolder = gui,addFolder("axis");
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  chad.rotation.y += 0.03;
+  chad.rotation.z += 0.03;
+
+  camera.position.z = t * 0.01;
+  camera.position.y = t * 0.0002;
+  camera.position.x = t * 0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
 
 function animate() {
   requestAnimationFrame(animate);
@@ -108,13 +135,14 @@ function animate() {
   torus.rotation.y += 0.06;
   torus.rotation.z += 0.01;
 
-  moon.rotateOnAxis(axis, speed);
-  chad.rotateOnAxis(axis, speed);
-  // chad.rotation.y += 0.02;
-  // chad.rotation.x += 0.02;
-  // moon.rotation.y += 0.02;
+  moon.rotation.x += 0.01;
 
-  controls.update();
+  // moon.rotateOnAxis(axis, speed);
+  // chad.rotateOnAxis(axis, speed);
+  chad.rotation.y += 0.02;
+  chad.rotation.x += 0.02;
+
+  // controls.update();
 
   renderer.render(scene, camera);
 }
